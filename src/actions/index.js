8164721053dd045c9067a3de8c7a44dc26215e0f
danrodz <daniel.a.rodriguez@dr.com>
2018-10-28 11:16:28 -0500

    added redux-promise middleware and google maps

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-const API_KEY = '0831a14e659349d397fa214d14bc4a0e';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,11 +1,10 @@
 import { FETCH_WEATHER } from '../actions/index';
 
-export default function(state = [], action) {
+export default function cb(state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
       return [action.payload.data, ...state];
-
     default:
-      return state;
   }
+  return state;
 }
